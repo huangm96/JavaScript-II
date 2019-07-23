d95d79d055eec7c060eddf877a0e230b44b0ee3f
unknown <minh.huang96@gmail.com>
2019-07-23 12:13:12 -0700

    finished closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,7 +1,16 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
-
+function house(){
+ const item1 ="sofa";
+ const item2 ="table";
+ function room(){
+   console.log(item1 +" "+item2);
+   console.log("bed");
+   console.log("computer");
+ }
+ room();
+};
+house();
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
